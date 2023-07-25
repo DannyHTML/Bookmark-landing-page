fdedee0663e42c45a189a660fec35296ff498d14
DannyHTML <dannyjag@gmail.com>
2023-07-25 17:10:52 +0200

    changes

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import { createRouter, createWebHashHistory } from "vue-router";
 import { Collapse } from "vue-collapsed";
 import "./style.css";
-import "./assets/custom-btn-style.css";
+import "./assets/custom-style.css";
 import App from "./App.vue";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { library } from "@fortawesome/fontawesome-svg-core";
