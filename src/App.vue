1687ef138bd6f0c8466f5d31b5f2a193840a02a6
DannyHTML <dannyjag@gmail.com>
2023-07-25 17:54:48 +0200

    uninstall vue/motion

--- src/App.vue
+++ src/App.vue
@@ -10,27 +10,29 @@ import Footer from "./Footer.vue";
 </script>
 
 <template>
-  <div class="pb-24 md:pb-28">
-    <Header />
+  <div>
+    <div class="pb-24 md:pb-28">
+      <Header />
+    </div>
+    <div class="mb-28">
+      <FirstSection />
+    </div>
+    <Features />
+    <CustomLinks />
+    <router-view class="mb-28" v-slot="{ Component }">
+      <transition name="slide" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
+    <div class="mb-20">
+      <Extensions />
+    </div>
+    <div class="mb-28">
+      <Faq />
+    </div>
+    <Contact />
+    <Footer />
   </div>
-  <div class="mb-28">
-    <FirstSection />
-  </div>
-  <Features />
-  <CustomLinks />
-  <router-view class="mb-28" v-slot="{ Component }">
-    <transition name="slide" mode="out-in">
-      <component :is="Component" />
-    </transition>
-  </router-view>
-  <div class="mb-20">
-    <Extensions />
-  </div>
-  <div class="mb-28">
-    <Faq />
-  </div>
-  <Contact />
-  <Footer />
 </template>
 
 <style scoped></style>
