105eb5fe29a207df0f8e9a8e966f5a7db9fabefc
Danny <dannyjag@gmail.com>
2023-11-16 17:09:36 +0100

    removed comment

--- src/components/CustomButton.vue
+++ src/components/CustomButton.vue
@@ -10,10 +10,3 @@ const props = defineProps<{
 }>();
 
 </script>
-
-
-<!-- rounded-lg uppercase bg-secondaryColour duration-200 flex justify-center items-center text-white w-24 h-10 py-2 cursor-pointer hover:bg-white border-2 border-secondaryColour hover:text-secondaryColour -->
-
-
-<!-- mobile  -->
-<!-- rounded-lg uppercase bg-secondaryColour duration-200 flex justify-center items-center text-white w-24 h-10 py-2 cursor-pointer hover:bg-white border-2 border-secondaryColour hover:text-secondaryColour -->
